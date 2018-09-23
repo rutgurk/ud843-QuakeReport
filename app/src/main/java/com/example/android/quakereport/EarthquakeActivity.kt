35e2e679c1d9ee58e5071a6c8a6e4be673bcdfd6
Rutger Korstjens <rutger.korstjens@gmail.com>
2018-09-23 16:12:40 +0200

    added empty state, loader and connection check
    added view to cover scenario's where loading takes some time, data is not retreived or internet is unavailable

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.kt
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.kt
@@ -16,20 +16,25 @@
 package com.example.android.quakereport
 
 import android.app.LoaderManager
+import android.content.Context
 import android.content.Intent
 import android.content.Loader
+import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 
 class EarthquakeActivity : LoaderManager.LoaderCallbacks<List<EarthQuake>>, AppCompatActivity() {
 
     private val USGS_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10"
     private val EARTHQUAKE_LOADER_ID = 1
+    private lateinit var textViewForEmptyEarthQuakeListViewState: TextView
+    lateinit var adapter: QuakeAdapter
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<List<EarthQuake>> {
         Log.i(LOG_TAG, "Loader info: onCreateLoader has been called")
@@ -38,6 +43,9 @@ class EarthquakeActivity : LoaderManager.LoaderCallbacks<List<EarthQuake>>, AppC
 
     override fun onLoadFinished(loader: Loader<List<EarthQuake>>, data: List<EarthQuake>?) {
         Log.i(LOG_TAG, "Loader info: onLoadFinished has been called")
+        val loadingIndicator = findViewById<View>(R.id.loading_indicator)
+        loadingIndicator.visibility = View.GONE
+        textViewForEmptyEarthQuakeListViewState.setText(R.string.no_earthquakes)
         adapter.clear()
         // If there is no result, do nothing.
         if (data != null && !data.isEmpty()) {
@@ -51,13 +59,15 @@ class EarthquakeActivity : LoaderManager.LoaderCallbacks<List<EarthQuake>>, AppC
         adapter.clear()
     }
 
-    lateinit var adapter: QuakeAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.earthquake_activity)
+
+
         // Find a reference to the {@link ListView} in the layout
         val earthquakeListView = findViewById<View>(R.id.list) as ListView
+        textViewForEmptyEarthQuakeListViewState = findViewById(R.id.empty_view)
+        earthquakeListView.setEmptyView(textViewForEmptyEarthQuakeListViewState)
 
         // Create a new {@link ArrayAdapter} of earthquakes
         adapter = QuakeAdapter(
@@ -76,12 +86,21 @@ class EarthquakeActivity : LoaderManager.LoaderCallbacks<List<EarthQuake>>, AppC
                             show() }
             }
         }
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this) //.forceLoad()
-        Log.i(LOG_TAG, "Loader info: Loader has been initialized")
+
+        var connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var networkInfo = connectivityManager.activeNetworkInfo
+        if (networkInfo != null && networkInfo.isConnected) {
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this) //.forceLoad()
+            Log.i(LOG_TAG, "Loader info: Loader has been initialized")
+        } else {
+            //Should this be moved to onloadfinished?
+            val loadingIndicator = findViewById<View>(R.id.loading_indicator)
+            loadingIndicator.visibility = View.GONE
+            textViewForEmptyEarthQuakeListViewState.setText(R.string.no_internet_connection)
+        }
     }
 
     companion object {
-
         val LOG_TAG = EarthquakeActivity::class.simpleName
     }
 }
